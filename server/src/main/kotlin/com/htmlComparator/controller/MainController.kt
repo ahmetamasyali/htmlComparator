9674db6538de932887683156fe38e3eddc30a0f4
amasyali <ahmet.amasyali@sahibinden.com>
2020-02-27 11:11:30 +0300

    cross origin support added

--- server/src/main/kotlin/com/htmlComparator/controller/MainController.kt
+++ server/src/main/kotlin/com/htmlComparator/controller/MainController.kt
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 class MainController {
