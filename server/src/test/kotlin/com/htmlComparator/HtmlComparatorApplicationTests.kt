4190fa773a90afcc6fa15e164645f2dcd8e55823
amasyali <ahmet.amasyali@sahibinden.com>
2020-02-27 09:02:51 +0300

    htmlComparator backend

---
+++ server/src/test/kotlin/com/htmlComparator/HtmlComparatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.htmlComparator.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HtmlComparatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
